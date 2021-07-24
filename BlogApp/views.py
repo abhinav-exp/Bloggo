f3912c9de6717c15aee907c4f0a62be9dd6debd6
Abhinav <kabhinav482@gmail.com>
2021-07-24 18:01:57 +0530

    templates created

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -23,23 +23,23 @@ class PostDetailView(DetailView):
 
 class CreatePostView(LoginRequiredMixin, CreateView):
     login_url = '/login/'
-    redirect_field_name = 'post_detail.html'
+    redirect_field_name = 'BlogApp/post_detail.html'
     form_class = PostForm
     model = Post
 
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     login_url = '/login/'
-    redirect_field_name = 'post_detail.html'
+    redirect_field_name = 'BlogApp/post_detail.html'
     form_class = PostForm
     model = Post
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
-    success_url = reverse_lazy('post_list')
+    success_url = reverse_lazy('BlogApp/post_list')
 
 class DraftListView(LoginRequiredMixin, ListView):
     login_url = '/login/'
-    redirect_field_name = 'post_list.html'
+    redirect_field_name = 'BlogApp/post_list.html'
     model = Post
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('create_date')
